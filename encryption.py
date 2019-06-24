e8f3333f0fb467b2ed1b79b452fdc8b4304faf20
Xa1n <xhs893@gmail.com>
2019-06-24 13:53:36 +0100

    Fix If statement

--- encryption.py
+++ encryption.py
@@ -5,13 +5,13 @@ choice = ""
 while choice != '-1':
     choice = input("\nDo you want to encrypt or decrypt a message?\nEnter 1 to Encrpyt, 2 to Decrypt, -1 to Exit: ")
 
-if choice == '1':
-    message = input("\nEnter the message you want to encrypt: ")
+    if choice == '1':
+        message = input("\nEnter the message you want to encrypt: ")
 
-    for i in range(0, len(message)):
-        result = result + chr(ord(message[i]) - 2)
+        for i in range(0, len(message)):
+            result = result + chr(ord(message[i]) - 2)
 
-    print (result + "\n\n")
-    result = ""
+        print (result + "\n\n")
+        result = ""
 
     
